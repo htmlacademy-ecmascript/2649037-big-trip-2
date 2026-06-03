f7fa1dd93cd8931500e1e47415216fc6acce855b
IEROGLIF <ieroglif.kerch@gmail.com>
2026-06-03 18:18:31 +0300

    - добавил длительность в точках маршрута - пофиксил баги

--- src/utils.js
+++ src/utils.js
@@ -26,4 +26,24 @@ function updateItem(items, update) {
   return items.map((item) => item.id === update.id ? update : item);
 }
 
-export { getRandomNumber, getRandomOffers, isEsc, updateItem };
+function getDuration(start, end) {
+  const totalMinutes = end.diff(start, 'minute');
+  const formatTwoDigits = (number) => String(number).padStart(2, '0');
+  let duration = '';
+
+  if (totalMinutes < 60) {
+    duration = `${totalMinutes}M`;
+  } else if (totalMinutes < 24 * 60) {
+    const hours = Math.floor(totalMinutes / 60);
+    const minutes = totalMinutes % 60;
+    duration = `${formatTwoDigits(hours)}H ${formatTwoDigits(minutes)}M`;
+  } else {
+    const days = Math.floor(totalMinutes / (24 * 60));
+    const hours = Math.floor((totalMinutes % (24 * 60)) / 60);
+    const minutes = totalMinutes % 60;
+    duration = `${formatTwoDigits(days)}D ${formatTwoDigits(hours)}H ${formatTwoDigits(minutes)}M`;
+  }
+  return duration;
+}
+
+export { getRandomNumber, getRandomOffers, isEsc, updateItem, getDuration };
